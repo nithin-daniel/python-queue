8021e7383a600ef0b18e4465b4b8937ed9f29002
Nithin Daniel <66398646+nithin-daniel@users.noreply.github.com>
2024-07-17 17:22:32 +0530

    fix:removed commented lines

--- main.py
+++ main.py
@@ -5,37 +5,7 @@ import threading
 
 app = Flask(__name__)
 
-# # Create a priority queue
-# pq = []
 
-# # Function to process tasks
-# def process_tasks():
-#     while True:
-#         if pq:
-#             task = heapq.heappop(pq)
-#             print(f"Processing task: {task}")
-#             time.sleep(task[2])
-#         else:
-#             time.sleep(1)  # Sleep for a second if the queue is empty
-
-# # Start the task processing in a separate thread
-# threading.Thread(target=process_tasks, daemon=True).start()
-
-# @app.route('/add_task', methods=['POST'])
-# def add_task():
-#     data = request.json
-#     if 'priority' in data and 'name' in data and 'duration' in data:
-#         task = (data['priority'], data['name'], data['duration'])
-#         heapq.heappush(pq, task)
-#         return jsonify({"message": "Task added successfully"}), 201
-#     return jsonify({"error": "Invalid task data"}), 400
-
-# @app.route('/get_tasks', methods=['GET'])
-# def get_tasks():
-#     return jsonify(list(pq))
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
 
 import heapq
 import time
